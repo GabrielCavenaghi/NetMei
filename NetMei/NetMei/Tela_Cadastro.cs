92c8471ad383f627578992acc5481763751ce507
GabrielCavenaghi <gabriel.cavenaghi75@gmail.com>
2024-11-17 11:59:31 -0300

    Add files via upload

---
+++ NetMei/NetMei/Tela_Cadastro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetMei
+{
+    public partial class Tela_Cadastro : Form
+    {
+        public Tela_Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Cadastrar_Click(object sender, EventArgs e)
+        {
+            if(tBox_Senha.Text == tBox_senhaConfirma.Text)
+            {
+                
+            }
+            else
+            {
+                MessageBox.Show("Confirme Sua senha");
+            }
+        }
+    }
+}
---
+++ NetMei/NetMei/Tela_ConfigUser_Mei.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetMei
+{
+    public partial class Tela_ConfigUser_Mei : Form
+    {
+        public Tela_ConfigUser_Mei()
+        {
+            InitializeComponent();
+        }
+    }
+}
