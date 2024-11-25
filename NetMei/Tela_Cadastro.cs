038a673793b0f58534dd3d995829635343984c2c
Guilherme Pelegrini <guipelegrini2014@gmail.com>
2024-11-25 07:17:45 -0300

    Só atualiza aq o C# ent

---
+++ NetMei/Tela_Cadastro.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace NetMei
+{
+    public partial class Tela_Cadastro : Form
+    {
+        public Tela_Cadastro()
+        {
+            InitializeComponent();
+            MySqlConnection conectar = new MySqlConnection("SERVER=localhost;DATABASE=netmei;UID=root;PASSWORD= ");
+            conectar.Open();
+            MySqlCommand consulta = new MySqlCommand();
+            consulta.Connection = conectar;
+            consulta.CommandText = "SELECT * FROM permissao";
+
+            MySqlDataReader resultado = consulta.ExecuteReader();
+            if (resultado.HasRows)
+            {
+                while (resultado.Read())
+                {
+                    string permissao = resultado["permissao"].ToString();
+                    if (permissao != "adm") // Verifica se a permissão não é "adm"
+                    {
+                        comboBox1.Items.Add(permissao); // Adiciona apenas permissões permitidas
+                    }
+                   
+                }
+                resultado.Close();
+            }
+            else
+            {
+                MessageBox.Show("Erro ao carregar as permissões.");
+            }
+            conectar.Close();
+        
+    }
+
+
+        private void btn_Cadastrar_Click(object sender, EventArgs e)
+        {
+            if (tBox_Senha.Text == tBox_senhaConfirma.Text)
+            {
+
+
+                MySqlConnection conexao = new MySqlConnection();
+                conexao.ConnectionString = "SERVER=localhost;DATABASE=netmei;UID=root;PASSWORD= ; ";
+                conexao.Open();
+
+                MySqlConnection conexao2 = new MySqlConnection();
+                conexao2.ConnectionString = "SERVER=localhost;DATABASE=netmei;UID=root;PASSWORD= ; ";
+                conexao2.Open();
+
+                string inserir = "INSERT INTO usuario(nome, email, senha, telefone) values('" + tBox_Nome.Text + "','" + tBox_Email.Text + "','" + tBox_Senha.Text + "','" + tBox_Telefone.Text + "');";
+                MySqlCommand comandos = new MySqlCommand(inserir, conexao);
+                comandos.ExecuteNonQuery();
+
+                MySqlCommand consulta = new MySqlCommand();
+                consulta.Connection = conexao;
+                consulta.CommandText = "SELECT * FROM usuario WHERE usuario.nome = '" + tBox_Nome.Text + "'AND usuario.email = '" + tBox_Email.Text + "' AND usuario.senha = '" + tBox_Senha.Text + "' AND usuario.telefone = '" + tBox_Telefone.Text + "' ;";
+                MySqlDataReader resultado = consulta.ExecuteReader();
+
+                MySqlCommand consulta2 = new MySqlCommand();
+                consulta2.Connection = conexao2;
+                consulta2.CommandText = "SELECT * FROM permissao WHERE permissao.permissao = '" + comboBox1.Text + "';";
+                MySqlDataReader resultado2 = consulta2.ExecuteReader();
+
+
+                string id_usuario = ""; //(id_pessoa)
+                string id_permissao = ""; //(id_festa)
+                string[] split = comboBox1.Text.Split('-');
+
+                id_usuario = split[0];
+                if (resultado.HasRows)
+                {
+                    while (resultado.Read())
+                    {
+                        id_usuario = resultado[0].ToString();
+                    }
+                }
+
+                id_permissao = split[0];
+                if (resultado2.HasRows)
+                {
+                    while (resultado2.Read())
+                    {
+                        id_permissao = resultado2[0].ToString();
+                    }
+                }
+
+                conexao.Close();
+                conexao2.Close();
+                conexao.Open();
+
+                string inserir2 = "INSERT INTO usuario_permissao(id_usuario_id, id_permissao_id) values(" + id_usuario + "," + id_permissao + ")";
+                MySqlCommand comandos2 = new MySqlCommand(inserir2, conexao);
+                comandos2.ExecuteNonQuery();
+
+                conexao.Close();
+
+                MessageBox.Show("Cadastro realizado com sucesso");
+                tBox_Nome.Text = "";
+                tBox_Email.Text = "";
+                tBox_Senha.Text = "";
+                tBox_Telefone.Text = "";
+                tBox_senhaConfirma.Text = "";
+                comboBox1.ResetText();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Confirme sua senha");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Tela_Entrar novaAba = new Tela_Entrar();
+            novaAba.Show();
+        }
+    }
+}
---
+++ NetMei/Tela_Entrar.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace NetMei
+{
+    partial class Tela_Entrar
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Entrar = new System.Windows.Forms.Button();
+            this.btn_CriarConta = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Entrar
+            // 
+            this.btn_Entrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Entrar.Location = new System.Drawing.Point(242, 219);
+            this.btn_Entrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Entrar.Name = "btn_Entrar";
+            this.btn_Entrar.Size = new System.Drawing.Size(74, 33);
+            this.btn_Entrar.TabIndex = 0;
+            this.btn_Entrar.Text = "Entrar";
+            this.btn_Entrar.UseVisualStyleBackColor = true;
+            this.btn_Entrar.Click += new System.EventHandler(this.btn_Entrar_Click);
+            // 
+            // btn_CriarConta
+            // 
+            this.btn_CriarConta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_CriarConta.Location = new System.Drawing.Point(218, 304);
+            this.btn_CriarConta.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_CriarConta.Name = "btn_CriarConta";
+            this.btn_CriarConta.Size = new System.Drawing.Size(116, 33);
+            this.btn_CriarConta.TabIndex = 1;
+            this.btn_CriarConta.Text = "Criar um conta";
+            this.btn_CriarConta.UseVisualStyleBackColor = true;
+            this.btn_CriarConta.Click += new System.EventHandler(this.btn_CriarConta_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(214, 281);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Não tem uma conta?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(233, 125);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(91, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(233, 181);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(91, 26);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(232, 102);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(232, 158);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Senha:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(236, 39);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Login";
+            // 
+            // Tela_Entrar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_CriarConta);
+            this.Controls.Add(this.btn_Entrar);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Tela_Entrar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Entrar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Entrar;
+        private System.Windows.Forms.Button btn_CriarConta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
